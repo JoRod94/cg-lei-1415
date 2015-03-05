cf8d52a3378a11fa97f004b67a40104d9127c132
Fernando Mendes <fjrib.mendes@gmail.com>
2015-03-05 18:14:05 +0000

    Drafts engine .3d file parser

--- engine/engine/engine.cpp
+++ engine/engine/engine.cpp
@@ -3,14 +3,59 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "point.h"
 
 using namespace std;
 
+
+void read_file(string name) {
+	ifstream file;
+	file.open(name);
+
+	string line,
+		   drawing,
+		   _nr_points;
+
+	getline(file, line);
+
+	istringstream ss(line);
+
+	getline(ss, drawing, ',');
+	getline(ss, _nr_points);
+
+	int nr_points = stoi(_nr_points);
+
+	if (nr_points % 3) {
+		std::cout << "Invalid number of points" << endl;
+		return;
+	}
+
+	for (int i = 0; i < nr_points; i += 3) {
+		float x, y, z;
+		string token;
+		getline(file, line);
+		istringstream stream(line);
+		getline(stream, token, ',');
+		x = stof(token);
+		getline(stream, token, ',');
+		y = stof(token);
+		getline(stream, token);
+		z = stof(token);
+		point p(x, y, z);
+		cout << x << " " << y << " " << z << endl;
+	}
+
+	cout << "NR POINTS READ: " << nr_points << " TYPE OF FIGURE: " << drawing << endl;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Hello world! This is engine! Press enter to exit..." << endl;
-	int x;
-	cin >> x;
+	cout << "Hello world! This is engine! Reading from test.txt" << endl;
+	read_file("test.txt");
 	return 0;
 }
 
