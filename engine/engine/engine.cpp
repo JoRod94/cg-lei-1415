bd868a0de0e5846bc0fc1fc55bbbfe9b0ab166a3
Fernando Mendes <fjrib.mendes@gmail.com>
2015-03-05 16:14:52 +0000

    Adds engine project

---
+++ engine/engine/engine.cpp
@@ -0,0 +1,16 @@
+// engine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world! This is engine! Press enter to exit..." << endl;
+	int x;
+	cin >> x;
+	return 0;
+}
+
