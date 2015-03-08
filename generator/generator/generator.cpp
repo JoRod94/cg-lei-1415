d417871b6b6ee93edb7b1d3dc79ef94cddd4a967
tiagoddinis <tiagoddinis@hotmail.com>
2015-03-08 13:26:46 +0000

    Added plane creation(incomplete)

--- generator/generator/generator.cpp
+++ generator/generator/generator.cpp
@@ -10,22 +10,28 @@
 
 //using namespace std;
 
-void create_plane(float length, float width, std::vector<float>& vec){
+void create_plane(float length, float width){
+	std::vector<point> points;
+	int triangle_point_index[6], i = 0;
+
 	//first triangle
 	point p1(0, 0, 0);
-	p1.add_to(vec);
+	points.push_back(p1);
+	triangle_point_index[0];
 	point p2(0, 0, length);
-	p2.add_to(vec);
+	points.push_back(p2);
+	triangle_point_index[1];
 	point p3(width, 0, length);
-	p3.add_to(vec);
+	points.push_back(p3);
+	triangle_point_index[2];
 
 	//second triangle
-	point p4(width, 0, length);
-	p4.add_to(vec);
-	point p5(width, 0, 0);
-	p5.add_to(vec);
-	point p6(0, 0, 0);
-	p6.add_to(vec);
+	triangle_point_index[2];
+	point p4(width, 0, 0);
+	points.push_back(p4);
+	triangle_point_index[3];
+	triangle_point_index[0];
+
 	std::cout << "myvector stores " << int(vec.size()) << " numbers.\n";
 }
 
@@ -36,14 +42,14 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	else if (strcmp(argv[1], "plane") == 0){
-		if (argc < 4){
-			cout << "Too few arguments";
+		if (argc != 5){
+			cout << "Must receive 5 arguments";
 			return 0;
 		}
 		cout << "Generating Plane... \n";
-		writePlane(atoi(argv[2]), atoi(argv[3]));
+		create_plane(atoi(argv[2]), atoi(argv[3]));
 		cout << "Done \n";
-	}
+	}/*
 	else if (strcmp(argv[1], "sphere") == 0){
 		if (argc < 5){
 			cout << "Too few arguments";
@@ -70,7 +76,7 @@ int main(int argc, char* argv[])
 		cout << "Generating Cone... \n";
 		writeCone(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
 		cout << "Done \n";
-	}
+	}*/
 	else
 		cout << "Command not recognized";
 }
