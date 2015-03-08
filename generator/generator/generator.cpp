a7288d1b7dc9fa58191e23130e076707ace3c8e5
tiagoddinis <tiagoddinis@hotmail.com>
2015-03-08 19:20:27 +0000

    added plane creation

--- generator/generator/generator.cpp
+++ generator/generator/generator.cpp
@@ -8,31 +8,30 @@
 #include <point.h>
 #include <stdlib.h>
 
-//using namespace std;
+using namespace std;
 
-void create_plane(float length, float width){
-	std::vector<point> points;
-	int triangle_point_index[6], i = 0;
+void create_plane(float length, float width, vector<point> points, int tpi[6]){
+	int i = 0;
 
 	//first triangle
 	point p1(0, 0, 0);
 	points.push_back(p1);
-	triangle_point_index[0];
+	tpi[0];
 	point p2(0, 0, length);
 	points.push_back(p2);
-	triangle_point_index[1];
+	tpi[1];
 	point p3(width, 0, length);
 	points.push_back(p3);
-	triangle_point_index[2];
+	tpi[2];
 
 	//second triangle
-	triangle_point_index[2];
+	tpi[2];
 	point p4(width, 0, 0);
 	points.push_back(p4);
-	triangle_point_index[3];
-	triangle_point_index[0];
+	tpi[3];
+	tpi[0];
 
-	std::cout << "myvector stores " << int(vec.size()) << " numbers.\n";
+	//std::cout << "myvector stores " << int(vec.size()) << " numbers.\n";
 }
 
 int main(int argc, char* argv[])
@@ -47,7 +46,10 @@ int main(int argc, char* argv[])
 			return 0;
 		}
 		cout << "Generating Plane... \n";
-		create_plane(atoi(argv[2]), atoi(argv[3]));
+		std::vector<point> points;
+		int triangle_point_index[6];
+		create_plane(atoi(argv[2]), atoi(argv[3]), points, triangle_point_index);
+		//create_file(argv[4], points, triangle_point_index);
 		cout << "Done \n";
 	}/*
 	else if (strcmp(argv[1], "sphere") == 0){
