ee288384c817fe39f71d739b98b1876c29dfb02a
João Rodrigues <joao.rod94@gmail.com>
2015-05-04 02:00:33 +0100

    Translation fix

--- engine/engine/translation.cpp
+++ engine/engine/translation.cpp
@@ -19,19 +19,25 @@ void getCatmullRomPoint(float t, int *indices, float *res, float *der, vector<po
 
 
 
-	float c;
+	float c, c2;
 	for (int j = 0; j < 4; j++){
 		c = 0;
+		c2 = 0;
 		for (int i = 0; i < 4; i++){
 			c += pow(t, (3 - i)) * m[i][j];
+			if (i < 3) c2 += (3 - i)*pow(t, (2 - i))*m[i][j];
 		}
 		temp[j] = c;
-		temp2[j] = 3 * pow(t, 2) * m[0][j] + 2 * t * m[1][j] + m[2][j];
+		temp2[j] = c2;
 	}
 
 	res[0] = temp[0] * p[indices[0]].x + temp[1] * p[indices[1]].x + temp[2] * p[indices[2]].x + temp[3] * p[indices[3]].x;
 	res[1] = temp[0] * p[indices[0]].y + temp[1] * p[indices[1]].y + temp[2] * p[indices[2]].y + temp[3] * p[indices[3]].y;
-	res[1] = temp[0] * p[indices[0]].z + temp[1] * p[indices[1]].z + temp[2] * p[indices[2]].z + temp[3] * p[indices[3]].z;
+	res[2] = temp[0] * p[indices[0]].z + temp[1] * p[indices[1]].z + temp[2] * p[indices[2]].z + temp[3] * p[indices[3]].z;
+
+	der[0] = temp2[0] * p[indices[0]].x + temp2[1] * p[indices[1]].x + temp2[2] * p[indices[2]].x + temp2[3] * p[indices[3]].x;
+	der[1] = temp2[0] * p[indices[0]].y + temp2[1] * p[indices[1]].y + temp2[2] * p[indices[2]].y + temp2[3] * p[indices[3]].y;
+	der[2] = temp2[0] * p[indices[0]].z + temp2[1] * p[indices[1]].z + temp2[2] * p[indices[2]].z + temp2[3] * p[indices[3]].z;
 
 }
 
@@ -59,7 +65,7 @@ void renderCatmullRomCurve(float *res, float *der, int pointCount, vector<point>
 	while (gt < 1.0f){
 		getGlobalCatmullRomPoint(gt, res, der, p);
 		glVertex3f(res[0], res[1], res[2]);
-		gt += 0.001f;
+		gt += 0.0001f;
 	}
 	glEnd();
 }
@@ -70,31 +76,28 @@ void normalize(float* v, int n) {
 		v[i] /= norm;
 }
 
-float * normalizedCrossProduct(float *a, float *b){
-	float result[3];
-	result[0] = a[1] * b[2] - b[1] * a[2];
-	result[1] = a[2] * b[0] - b[2] * a[0];
-	result[2] = a[0] * b[1] - b[0] * a[1];
-	normalize(result, 3);
-	return result;
+void crossProduct(float *r, float *a, float *b){
+	
+	r[0] = a[1] * b[2] - b[1] * a[2];
+	r[1] = a[2] * b[0] - b[2] * a[0];
+	r[2] = a[0] * b[1] - b[0] * a[1];
 }
 
-void curveRotation(float *der, float *p, float *up){
+void curveRotation(float *der, float *up){
 
-	float *left;
+	float left[3];
 	
 	normalize(der, 3);
-	left = normalizedCrossProduct(up, der);
-	up = normalizedCrossProduct(der, left);
+	crossProduct(left, up, der);
+	normalize(left, 3);
+	crossProduct(up, der, left);
+	normalize(up, 3);
 
-	float m[16] = { left[0], left[1], left[2], 0, up[0], up[1], up[2], 0, der[0], der[1], der[2], 0, p[0], p[1], p[2], 1 };
+	float m[16] = { left[0], left[1], left[2], 0, up[0], up[1], up[2], 0, der[0], der[1], der[2], 0,0.0f,0.0f,0.0f, 1 };
 	glMultMatrixf(m);
 }
 
 void Translation::apply(){
-	for (int i = 0; i < points.size(); i++){
-		std::cout << points[i].x << ", " << points[i].y << ", " << points[i].z << std::endl;
-	}
 
 	//Normal Translation
 	if(time == 0)
@@ -106,9 +109,10 @@ void Translation::apply(){
 		renderCatmullRomCurve(res, der, points.size(), points);
 
 		getGlobalCatmullRomPoint(t, res, der, points);
+
 		glTranslatef(res[0], res[1], res[2]);
-		float p[3] = { 0, 0, 0 };
-		curveRotation(der,p, up);
+
+		curveRotation(der, up);
 
 		t = fmod(glutGet(GLUT_ELAPSED_TIME) / (time*1000.0f), 1);
 	}
