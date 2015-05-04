e030068624a9fb0f333df9fbf47aa5cd3ab91472
João Rodrigues <joao.rod94@gmail.com>
2015-05-04 01:44:05 +0100

    Rotate and Curve, bad example

--- engine/engine/rotation.cpp
+++ engine/engine/rotation.cpp
@@ -1,11 +1,18 @@
 #include "stdafx.h"
 #include <GL/glut.h>
 #include "rotation.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
 
-
+using namespace std;
 
 void Rotation::apply(){
-	// TODO: Update this to the timed version
-	glRotatef(angle, x, y, z);
+	if (time == 0)
+		glRotatef(angle, x, y, z);
+	else{
+		float ang = (glutGet(GLUT_ELAPSED_TIME)*90) / (time*1000.0f);
+		glRotatef(ang, x, y, z);
+	}
 }
 
--- engine/toolkits/includes/rotation.h
+++ engine/toolkits/includes/rotation.h
@@ -10,7 +10,7 @@ public:
 	float angle;
 	long time;
 
-	Rotation(float angle, long time, float x, float y, float z) : angle(angle), time(time), x(x), y(y), z(z) {};
+	Rotation(long time, float angle, float x, float y, float z) : angle(angle), time(time), x(x), y(y), z(z) {};
 	Rotation(float angle, float x, float y, float z) : angle(angle), x(x), y(y), z(z), time(0) {};
 	Rotation(long time, float x, float y, float z) : time(time), x(x), y(y), z(z), angle(0) {};
 	Rotation() : angle(0), time(0), x(0), y(0), z(0) {};
