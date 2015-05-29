9ba01cbf17e176f780c6c364669d5d605091ffa5
Fernando Mendes <fjrib.mendes@gmail.com>
2015-05-29 12:08:18 +0100

    Fix grid color

--- engine/engine/engine.cpp
+++ engine/engine/engine.cpp
@@ -147,6 +147,12 @@ static void gridBoolInit(){
 }
 
 static void drawGrid(){
+	if (glIsEnabled(GL_LIGHTING)) {
+		float green[4] = { 0, 1.0f, 0, 1.0f };
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, green);
+	}
+	else
+		glColor3f(0, 1.0f, 0);
 	if (gridBools[0]){
 
 		if (gridBools[1]){
@@ -188,6 +194,7 @@ static void drawGrid(){
 				glEnd();
 			}
 		}
+	reset_color();
 	}
 }
 
